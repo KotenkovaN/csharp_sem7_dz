68a81fd903e6ee626394400e2eccc50fa5900059
Nataly <natalya-kotenkova@yandex.ru>
2022-12-15 15:09:27 +0100

    added task47

---
+++ task47/Program.cs
@@ -0,0 +1,17 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+double[] size = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
