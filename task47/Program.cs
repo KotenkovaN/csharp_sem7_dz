bb12a0fe36e369c7cfb099c7b4186ac935273936
Nataly <natalya-kotenkova@yandex.ru>
2022-12-16 09:43:23 +0100

    fixed task47

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,17 +1,19 @@
 ﻿void InputMatrix(double[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for ( int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(1, 11); 
+            matrix[i, j] = new Random().NextDouble(); 
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
     }
 }
+
 Console.Clear();
 Console.Write("Введите размеры матрицы: ");
-double[] size = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
-double[,] matrix = new double[size[0], size[1]];
+string[] numbers = Console.ReadLine().Split(" ");
+double[,] matrix = new double[int.Parse(numbers[0]), int.Parse(numbers[1])];
+Console.WriteLine("Двумерный массив: ");
 InputMatrix(matrix);
